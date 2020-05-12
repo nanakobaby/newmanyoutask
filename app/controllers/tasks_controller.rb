90bca5f778bdf2c47d97b2f5ad4f34ed82a550e3
nanako kan <n.k.0213.0529@gmail.com>
2020-05-12 22:56:19 +0900

    タスクに対し、ステータス登録を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -49,7 +49,7 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:task_name, :content, :end_on)
+    params.require(:task).permit(:task_name, :content, :end_on, :status)
   end
 
   def set_task
