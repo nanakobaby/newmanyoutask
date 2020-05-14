45b6c098812fc45c61cef668931792945901da9a
nanako kan <n.k.0213.0529@gmail.com>
2020-05-14 15:14:31 +0900

    ページネーションの追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,20 +4,21 @@ class TasksController < ApplicationController
   def index
     #params[:sort_expired]に値があれば、終了期限でソートした値を表示させる
     if params[:sort_expired]
-      @tasks = Task.all.order(end_on: :desc)
-      @tasks = Task.all.order(priority: :asc)
-    else
+      @tasks = Task.all.order(end_on: :desc).page(params[:page]).per(10)
+    elsif params[:sort_priority]
+      @tasks = Task.all.order(priority: :asc).page(params[:page]).per(10)
+    elsif
       #値がなければ、Task.allで取り出せるそのままの値を出力
-      @tasks = Task.all.order(created_at: :desc)
+      @tasks = Task.all.order(created_at: :desc).page(params[:page]).per(10)
     end
 
     if params[:search].present?
       if params[:task_name].present? && params[:status].present?
-        @tasks = Task.status(params[:status]).task_name_like(params[:task_name])
+        @tasks = Task.status(params[:status]).task_name_like(params[:task_name]).page(params[:page]).per(10)
       elsif params[:task_name].present?
-        @tasks = Task.task_name_like(params[:task_name])
+        @tasks = Task.task_name_like(params[:task_name]).page(params[:page]).per(10)
       elsif params[:status].present?
-        @tasks = Task.status(params[:status])
+        @tasks = Task.status(params[:status]).page(params[:page]).per(10)
       end
     end
   end
