09c331321f7a2bafdde3d260c3c10b1d9c7c56af
nanako kan <n.k.0213.0529@gmail.com>
2020-05-18 14:19:44 +0900

    現状のユーザ機能のsystem specを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
 class User < ApplicationRecord
-  has_many :tasks, dependent: :destroy
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  before_destroy :do_not_destroy_last_admin
+  has_many :tasks, dependent: :destroy
+
+  private
+  def do_not_destroy_last_admin
+    # コールバックで処理を停止したい時は、throw :abortする。selfをつけ、クラスメソッドを定義する。
+    throw(:abort) if User.where(admin: true).count <= 1 && self.admin?
+  end
 end
